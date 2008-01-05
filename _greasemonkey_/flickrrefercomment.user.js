79015c734764c7f0592e1a3dd170c19fb1a218ba
Pierre Andrews <pierre.andrews@gmail.com>
2008-01-05 17:12:16 +0100

    added photophlow, flexplore, and drewmyersphoto scripts.

--- _greasemonkey_/flickrrefercomment.user.js
+++ _greasemonkey_/flickrrefercomment.user.js
@@ -2,9 +2,9 @@
 // @name	Flickr Refer Comment
 // @namespace	http://6v8.gamboni.org/
 // @description Auto comment the place where you come from
-// @version        1.1
+// @version        1.2
 // @identifier	http://6v8.gamboni.org/IMG/js/flickrrefercomment.user.js
-// @date           2007-11-23
+// @date           2008-01-05
 // @creator        Pierre Andrews (mortimer.pa@free.fr)
 // @include http://*flickr.com/photos/*/*
 // @exclude http://*flickr.com/photos/*/*#preview
@@ -50,8 +50,8 @@
 		namespace: "http://6v8.gamboni.org/",
 		description: "auto comment the place where you come from",
 		identifier: "http://6v8.gamboni.org/IMG/js/flickrrefercomment.user.js",
-		version: "1.1",								// version
-		date: (new Date(2007, 11, 23))		// update date
+		version: "1.2",								// version
+		date: (new Date(2008, 01, 05))		// update date
 		.valueOf()
 	};
 
@@ -62,6 +62,8 @@
 	var RANDOM = FELLOW+1;
 	var SET = RANDOM+1;
 	var MAP = SET+1;
+	var PHOTOPHLOW = MAP+1;
+
 
 	var FlickrReferComment = function() {this.init();}
 
@@ -195,13 +197,31 @@
 						this.insertComment("Discovered using FlickrFox.",referrer);
 					} else if(referrer.indexOf("http://16.gmodules.com/ig/") >= 0) {
 						this.insertComment("Seen in my contacts' photos.",'');
-					} else if(referrer.indexOf('flickr.com') < 0) {
+					} else if(referrer.indexOf("www.drewmyersphoto.net/flickr_scripts/cie/") >= 0) {
+						this.insertComment("Seen in my contacts' Explore photos.",referrer);
+					}  else if(referrer.indexOf("www.drewmyersphoto.net/flickr_scripts/ycrf/") >= 0) {
+						this.insertComment("Seen in my contacts' favorites.",referrer);
+					} else if(referrer.indexOf("flexplore.raum-fuer-notizen.de/flexplore") >= 0) {
+						this.insertComment("Seen on flexplore.",referrer);
+					} 
+				//cases for photoflow
+					else if(referrer.indexOf('http://www.photophlow.com/flickr/group/') >= 0) {
+						var groupid = referrer.replace(/http:\/\/www.photophlow.com\/flickr\/group\//,'');
+						groupid = groupid.replace(/inner\//,'');
+						this.resolveGroupName(PHOTOPHLOW,"http://www.flickr.com/groups/"+groupid,referrer);
+					}
+					else if(referrer.indexOf('http://www.photophlow.com/flickr/user/') >= 0) {
+						var userid = referrer.replace(/http:\/\/www.photophlow.com\/flickr\/user\//,'');
+						userid = userid.replace(/inner\//,'');
+						this.resolveUserName(PHOTOPHLOW,"http://www.flickr.com/people/"+userid,referrer);
+					}
+				//fall back
+					else if(referrer.indexOf('flickr.com') < 0) {
 						if(matches = /http:\/\/(www.)?([^\/]+).*/.exec(referrer)) {
 							this.insertComment("Seen on "+matches[2],referrer);
 						} else 
 							this.insertComment("Seen on the Web.",referrer);
 					} 
-				//TODO: popular page, people page, favorites, home, last uploads
 			} else {
 				//we have no referer, but we can still try to guess
 				if(matches = /\/in\/pool-([^\/]*)\/?/.exec(document.location.pathname)) {
@@ -236,6 +256,36 @@
 			}
 		},
 		
+		resolveUserName: function(type,userurl,referrer) {
+			//Trick to do it using the flickr API with authentication already embeded in the page.
+			var self = this;
+			var listener = {
+				flickr_urls_lookupUser_onLoad: function(success, responseXML, responseText, params){
+					if(success) {
+						var rsp = responseText.replace(/<\?xml.*\?>/,'');
+						rsp = new XML(rsp);
+						switch(type) {
+							case PHOTOPHLOW:
+								msg = "Discovered in "+rsp..username+"'s";
+								if(msg.indexOf('photoflow') < 0 || msg.indexOf('Photoflow') < 0) {
+									msg += " photoflow";
+								}
+								msg += " room.";
+								referrer = referrer.replace(/inner\//,'');
+								break;
+						}
+						
+						self.insertComment(msg,referrer);
+					} else
+					GM_log("error looking for user "+referrer+':' + responseText+'.');
+				}
+			};
+			unsafeWindow.F.API.callMethod('flickr.urls.lookupUser', {
+					url:userurl				   
+						}, listener);
+		}, 
+
+		
 		resolveGroupName: function(type,groupurl,referrer) {
 			//Trick to do it using the flickr API with authentication already embeded in the page.
 			var self = this;
@@ -254,6 +304,14 @@
 								break;
 							case FELLOW:
 								msg = "Seen next to a fellow photo of "+rsp..groupname+".";
+								break;
+							case PHOTOPHLOW:
+								msg = "Discovered in the "+rsp..groupname;
+								if(msg.indexOf('photophlow') < 0 && msg.indexOf('Photophlow') < 0) {
+									msg += " photophlow";
+								}
+								msg += " room.";
+								referrer = referrer.replace(/inner\//,'');
 								break;
 						}
 						
@@ -281,12 +339,14 @@
 				case POOL:
 				case MAP:
 				case FELLOW:
+				case PHOTOPHLOW:
 					unsafeWindow.F.API.callMethod('flickr.urls.lookupGroup', {
 							url:groupurl				   
 														  }, listener);
 			}
 			
 		}, 
+
 		resolveSetName: function(id,referrer,type) {
 			//Trick to do it using the flickr API with authentication already embeded in the page.
 			var self = this;
